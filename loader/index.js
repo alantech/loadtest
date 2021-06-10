9dbe04a22d53309a9b8cfce81f17974702ac55fa
David Ellis <isv.damocles@gmail.com>
2021-06-09 23:01:27 -0700

    wip

--- loader/index.js
+++ loader/index.js
@@ -2,7 +2,7 @@
 
 const fetch = require('cross-fetch')
 
-const baseUrl = 'https://loadtest-3.alandeploy.com'
+const baseUrl = 'https://test-lb-4.alandeploy.com'
 
 const testPatterns = [
   // First field is # of milliseconds the server should burn
