6a5f107d2076f986d50bd3471e5918baed72adee
Alejandro Guillen <aguillenv93@gmail.com>
2021-07-13 17:04:31 +0200

    Restore index

--- loader/index.js
+++ loader/index.js
@@ -2,8 +2,7 @@
 
 const fetch = require('cross-fetch')
 
-// const baseUrl = `https://${process.argv[process.argv.length - 1]}.alandeploy.com`
-const baseUrl = `https://load-test.anycloudapp.com`
+const baseUrl = `https://${process.argv[process.argv.length - 1]}.alandeploy.com`
 
 const testPatterns = [
   // First field is # of milliseconds the server should burn
